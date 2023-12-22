9db3cf49e3d319f18b8c300ee1f3e769d1d3fa11
trepegorka <egorpotapko@mail.ru>
2023-12-22 04:01:47 +0100

    react lazy loading pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Counter} from "./components/Counter";
 import './index.scss'
+import {Routes, Route, Link} from 'react-router-dom'
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+
 const App = () => {
     return (
         <div className="app">
-            hello@@
-            <Counter></Counter>
+            <Link to={'/main'}>main</Link>
+            <Link to={'/about'}>about</Link>
+            <Suspense>
+                <Routes>
+                    <Route path={'/main'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
+            <Counter />
         </div>
     );
 };
