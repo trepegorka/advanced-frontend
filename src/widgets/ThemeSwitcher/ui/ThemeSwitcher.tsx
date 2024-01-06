0510bf2cf3104f2907ad2b653cdff582306d7857
trepegorka <egorpotapko@mail.ru>
2024-01-06 18:03:16 +0100

    Custom theme switcher

---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,32 @@
+import {classNames} from 'shared/lib/classNames/classNames'
+import cls from './ThemeSwitcher.module.scss'
+import {useTheme} from "app/providers/ThemeProvider";
+import Moon from 'shared/assets/icons/moon.svg'
+import Sun from 'shared/assets/icons/sun.svg'
+import {Theme} from "app/providers/ThemeProvider/lib/ThemeContext";
+
+interface ThemeSwitcherProps {
+    className?: string
+}
+
+export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
+    const {theme, changeTheme} = useTheme()
+
+
+    return (
+        <div className={classNames(cls.container, {}, [cls.buttonSpace, className])}>
+            <label className={cls.toggle} htmlFor={cls.switch}>
+                <input id={cls.switch} className={cls.input} type={'checkbox'} onChange={changeTheme}
+                       checked={theme === Theme.NORMAL}/>
+
+                <div className={classNames(cls.icon, {}, [cls.iconMoon])}>
+                    <Moon/>
+                </div>
+
+                <div className={classNames(cls.icon, {}, [cls.iconSun])}>
+                    <Sun/>
+                </div>
+            </label>
+        </div>
+    );
+};
