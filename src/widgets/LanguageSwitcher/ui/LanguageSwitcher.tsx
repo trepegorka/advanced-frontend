3289073ea02072ed0d86ac385d3de809b64dbf52
trepegorka <egorpotapko@mail.ru>
2024-01-06 18:04:30 +0100

    language switcher ru/en

---
+++ src/widgets/LanguageSwitcher/ui/LanguageSwitcher.tsx
@@ -0,0 +1,25 @@
+import {classNames} from 'shared/lib/classNames/classNames'
+import cls from './LanguageSwitcher.module.scss'
+import {useTranslation} from 'react-i18next'
+import {Button} from "widgets/Button";
+import {ThemeButton} from "widgets/Button/ui/Button";
+
+interface LanguageSwitcherProps {
+    className?: string
+}
+
+export const LanguageSwitcher = ({className}: LanguageSwitcherProps) => {
+    const {t, i18n} = useTranslation();
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+    }
+    return (
+        <Button
+            className={classNames(cls.LanguageSwitcher, {}, [className])}
+            theme={ThemeButton.CLEAR}
+            onClick={toggle}
+        >
+            {t('RU')}
+        </Button>
+    );
+};
